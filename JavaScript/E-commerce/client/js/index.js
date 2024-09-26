ec68a2576024e945575d76b851dd7c2be5204b12
ArturoRiera <arturonriera@gmail.com>
2024-09-25 22:54:44 -0300

    JavaScript Clase 2 Videos 1,4

--- JavaScript/E-commerce/client/js/index.js
+++ JavaScript/E-commerce/client/js/index.js
@@ -9,4 +9,32 @@ productos.forEach((product) =>{
     <p>${product.price} $<p>
     `;
     shopContent.append(content);
+
+    
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+
+    buyButton.addEventListener("click", ()=>{
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat){
+            cart.map((prod)=> {
+                if(prod.id === product.id){
+                    prod.quanty++;
+                    displayCartCouter();
+                }
+            })
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+            displayCartCouter();
+        }
+    });
     });
