7672160f179de115a41010d239a6e41e9b2807f6
ArturoRiera <arturonriera@gmail.com>
2024-11-04 23:20:57 -0300

    Python Clase 03

---
+++ Laboratorio IV/Clase01/tipo_str.py
@@ -0,0 +1,13 @@
+import math
+
+# Profundizando en el tipo String
+# Concatenacion automatica en Python
+
+variable = 'Adios'
+mensaje = 'Hola'' Alumnos ' + variable #Se puede poner o no un + entre cadenas pero si o si al concatenar una variable
+mensaje += ', Terminamos'
+# print(mensaje)
+
+# Se le puede pasar una clase, un modulo, un atributo, etc
+# La clase help esta incluida dentro de python para ayudar o documentacion (no se debe importar built-in)
+help(math.isnan) #Pedimos ayuda para saber de este metodo
